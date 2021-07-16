0ed8b5c867a24fb707b92784f63ee4be22779119
Vinicius Barcelos <josevsaraiva@gmail.com>
2021-07-16 19:39:41 -0300

    chore: add cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,4 +1,5 @@
 import "reflect-metadata";
+import cors from "cors";
 import express from "express";
 import "dotenv/config";
 
@@ -9,6 +10,7 @@ import "../../container";
 
 createConnection();
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 
